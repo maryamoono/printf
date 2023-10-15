1e5f59db26585debd0297f367c317a531cc213d6
Maryam <cakemariam1234@gmail.com>
2023-10-15 15:13:31 -0700

    lol

--- _printf.c
+++ _printf.c
@@ -1,66 +1,60 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - that function that used to print string or format string
+ * @format: its refers to letters thar used with strings
+ * Return: the same integer
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+        unsigned int m, s_coont, coont = 0;
+        va_list lol;
+        char * str;
+        
+        if (!format || (format[0] == '%' && !format[1]))
 	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+		va_end(lol);
+		return (-1);
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	if (!format[0])
+	{
+		va_end(lol);
+		return (0);
+	}
+        va_start(lol, format);
+        for (m = 0; format && format[m] != '\0'; m++)
+        {
+                if (format[m] != '%')
+                {
+                        putchar(format[m]);
+                }
+                else if (format[m] == '%' && format[m + 1] == 'c')
+                {
+                        putchar(va_arg(lol, int));
+                        m++;
+                }
+                else if (format[m + 1] == 's')
+                {
+                    str = va_arg(lol, char *);
+                    while(*str != '\0'){
+                        putchar(*str);
+                        str++;
+                        coont++;
+                    }
+                    coont--;
+                    m++;
+                }
+                else if (format[m + 1] == '%')
+                {
+                        putchar('%');
+                        m++;
+                }
+                else{
+                    putchar('%');
+                    putchar(format[++m]);
+                    coont ++;
+                }
+                coont += 1;
+        }
+        va_end(lol);
+        return (coont);
 }
---
+++ putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - knkndnd
+ * @n: knlnwl
+ * Return: mjbdw
+ */
+int _putchar(char n)
+{
+	return (write(1, &n, 1));
+}
